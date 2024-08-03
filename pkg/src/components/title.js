0ffcd779146b4ca39900f84fd57df28974a1bbbf
Hossam sulleman <sullemanhossam4@gmail.com>
2024-08-03 23:05:53 +0100

    push

--- pkg/src/components/title.js
+++ pkg/src/components/title.js
@@ -1,22 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Gradient from "ink-gradient";
-import BigText from "ink-big-text";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Gradient from 'ink-gradient';
+import BigText from 'ink-big-text';
 
 class Title extends React.Component {
   render() {
-    const { text, color } = this.props;
+    const {text, color} = this.props;
     return (
       <Gradient name={color}>
-        <BigText text={text} />
+        <BigText text={text}/>
       </Gradient>
     );
   }
 }
 
 Title.defaultProps = {
-  color: "blue",
-  text: "paket phileep" // Define default text prop
+  color: 'blue',
+  text: 'paket phileep' // Define default text prop
 };
 
 Title.propTypes = {
